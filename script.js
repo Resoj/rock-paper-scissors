742c345f73e6bed153e81a74e9b1d7d06c9a5149
Jose Cruz <resoj.cruz@gmail.com>
2024-01-09 20:37:01 -0500

    Complete playRound() function

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ function getComputerChoice(){
     }
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection){
+    let winner;
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+
+    if(playerSelection === computerSelection){
+        return "It's a tie! Play another round"
+    }
+    if(playerSelection === 'ROCK' && computerSelection === 'PAPER'){
+        return 'You Lose! Paper beats Rock'
+    }
+    if(playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
+        return 'You Win! Rock beats Scissors'
+    }
+    if(playerSelection === 'PAPER' && computerSelection === 'SCISSORS'){
+        return 'You Lose! Scissors beat Paper'
+    }
+    if(playerSelection === 'PAPER' && computerSelection === 'ROCK'){
+        return 'You Win! Paper beats Rock'
+    }
+    if(playerSelection === 'SCISSORS' && computerSelection === 'ROCK'){
+        return 'You Lose! Rock beats Scissors'
+    }
+    if(playerSelection === 'SCISSORS' && computerSelection === 'PAPER'){
+        return 'You Win! Scissors beat Paper'
+    }
+}
